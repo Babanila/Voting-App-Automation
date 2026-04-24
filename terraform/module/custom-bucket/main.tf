7896d5d830d394bb042acdbbfae69e489165b247
Babajide Williams <babanila@yahoo.com>
2026-04-24 13:24:34 +0200

    feat: Add bucket module

---
+++ terraform/module/custom-bucket/main.tf
@@ -0,0 +1,45 @@
+# Create S3 Bucket for State Storage
+resource "aws_s3_bucket" "tfstate" {
+  bucket        = var.name
+  force_destroy = true
+  tags          = var.tags
+}
+
+# Enable Versioning
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable Server-Side Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block Public Access
+resource "aws_s3_bucket_public_access_block" "tfstate" {
+  bucket                  = aws_s3_bucket.tfstate.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Create DynamoDB Table for State Locking
+resource "aws_dynamodb_table" "tflock" {
+  name         = "tflock-${var.name}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
